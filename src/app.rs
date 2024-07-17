f192469ffc3592a465cc4d81b7e331821939af32
Marat Bukharov <marat.buharov@gmail.com>
2024-07-17 05:09:40 +0300

    settings

--- src/app.rs
+++ src/app.rs
@@ -2,12 +2,15 @@ use crate::bot::Direction;
 use crate::images::Images;
 use base64::engine::general_purpose;
 use core::hash;
-use egui::{Color32, Image, Sense, TextureHandle, TextureOptions};
+use egui::{Color32, Image, Sense, Style, TextureHandle, TextureOptions, Visuals};
 use rand::{Fill, Rng, SeedableRng};
 use rand_seeder::SipHasher;
 use rand_xoshiro::Xoshiro256PlusPlus;
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 use std::io::Write;
+use std::time::Duration;
+use web_time::Instant;
 
 #[derive(Eq, PartialEq, Clone)]
 struct BotTextures {
@@ -23,6 +26,29 @@ struct ImageTextures {
     bot: BotTextures,
 }
 
+#[derive(Deserialize, Serialize, Eq, PartialEq, Copy, Clone, Debug)]
+enum AutoSaveSec {
+    One,
+    Five,
+    Fifteen,
+}
+
+impl AutoSaveSec {
+    fn duration(&self) -> Duration {
+        match self {
+            AutoSaveSec::One => Duration::from_secs(1),
+            AutoSaveSec::Five => Duration::from_secs(5),
+            AutoSaveSec::Fifteen => Duration::from_secs(15),
+        }
+    }
+}
+
+impl Display for AutoSaveSec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self.duration())
+    }
+}
+
 #[derive(Deserialize, Serialize, Eq, PartialEq)]
 #[serde(default)]
 pub struct TemplateApp {
@@ -33,6 +59,11 @@ pub struct TemplateApp {
     lock_initial_seed: bool,
     initial_seed: String,
     rng: Xoshiro256PlusPlus,
+    is_dark: bool,
+    show_settings: bool,
+    auto_save: Option<AutoSaveSec>,
+    #[serde(skip)]
+    last_save: Instant,
 }
 
 impl Default for TemplateApp {
@@ -45,6 +76,10 @@ impl Default for TemplateApp {
             lock_initial_seed: false,
             rng: new_seeded_rand(&initial_seed),
             initial_seed,
+            is_dark: Default::default(),
+            show_settings: Default::default(),
+            auto_save: Some(AutoSaveSec::Fifteen),
+            last_save: Instant::now(),
         }
     }
 }
@@ -56,11 +91,17 @@ impl TemplateApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
+        let result: TemplateApp = if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
         } else {
             Default::default()
-        }
+        };
+        cc.egui_ctx.set_visuals(if result.is_dark {
+            Visuals::dark()
+        } else {
+            Visuals::light()
+        });
+        result
     }
 
     fn images(&mut self, ui: &'_ mut egui::Ui) -> &ImageTextures {
@@ -92,22 +133,31 @@ impl TemplateApp {
             }
         })
     }
-}
 
-impl eframe::App for TemplateApp {
-    /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+    fn auto_save(&mut self, frame: &mut eframe::Frame) {
+        if let Some(storage) = frame.storage_mut() {
+            if let Some(auto_save) = self.auto_save {
+                if self.last_save.elapsed() >= auto_save.duration() {
+                    use eframe::App as _;
+                    self.save(storage);
+                }
+            }
+        }
+    }
+
+    fn top_menu(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.auto_save(frame);
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
+                egui::widgets::global_dark_light_mode_switch(ui);
+                ui.separator();
+
                 let is_web = cfg!(target_arch = "wasm32");
                 ui.menu_button("File", |ui| {
                     if let Some(storage) = frame.storage_mut() {
                         if ui.button("Save").clicked() {
+                            use eframe::App as _;
                             self.save(storage);
                             ui.close_menu();
                         }
@@ -121,11 +171,63 @@ impl eframe::App for TemplateApp {
                         }
                     }
                 });
-                ui.add_space(16.0);
-
-                egui::widgets::global_dark_light_mode_buttons(ui);
+                if ui.button("Settings").clicked() {
+                    self.show_settings ^= true;
+                }
             });
         });
+    }
+
+    pub fn global_dark_light_mode_switch(&mut self, ui: &mut egui::Ui) {
+        let style: Style = (*ui.ctx().style()).clone();
+        let new_visuals = style.visuals.light_dark_small_toggle_button(ui);
+        if let Some(visuals) = new_visuals {
+            self.is_dark = visuals.dark_mode;
+            ui.ctx().set_visuals(visuals);
+        }
+    }
+
+    fn settings_window(&mut self, ctx: &egui::Context) {
+        egui::Window::new("🔧 Settings")
+            .auto_sized()
+            .open(&mut self.show_settings)
+            .show(ctx, |ui| {
+                ui.vertical(|ui| {
+                    ui.checkbox(&mut self.lock_initial_seed, "Lock initial seed")
+                        .on_hover_text("Lock initial seed on start new simulation");
+                    ui.horizontal(|ui| {
+                        ui.label("Initial seed:");
+                        ui.text_edit_singleline(&mut self.initial_seed);
+                        if ui.button("⟳").clicked() {
+                            self.initial_seed = generate_initial_seed();
+                        }
+                    });
+                    ui.separator();
+                    ui.horizontal_top(|ui| {
+                        ui.label("Auto save:");
+                        ui.vertical(|ui| {
+                            ui.radio_value(&mut self.auto_save, None, "Disabled");
+                            let mut radio_value = |auto_save| {
+                                ui.radio_value(
+                                    &mut self.auto_save,
+                                    Some(auto_save),
+                                    format!("{}", auto_save),
+                                );
+                            };
+                            radio_value(AutoSaveSec::One);
+                            radio_value(AutoSaveSec::Five);
+                            radio_value(AutoSaveSec::Fifteen);
+                        });
+                    });
+                });
+            });
+    }
+}
+
+impl eframe::App for TemplateApp {
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        self.top_menu(ctx, frame);
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
@@ -158,36 +260,42 @@ impl eframe::App for TemplateApp {
                 }
             });
 
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/maratik123/biobots/blob/main/",
-                "Source code."
-            ));
-
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
+                bottom_frame(ui);
+                ui.separator();
             });
         });
+
+        self.settings_window(ctx);
     }
 
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
+        self.last_save = Instant::now();
     }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
+fn bottom_frame(ui: &mut egui::Ui) {
     ui.horizontal(|ui| {
+        ui.horizontal(|ui| {
+            ui.spacing_mut().item_spacing.x = 0.0;
+            ui.label("Powered by ");
+            ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+            ui.label(" and ");
+            ui.hyperlink_to(
+                "eframe",
+                "https://github.com/emilk/egui/tree/master/crates/eframe",
+            );
+            ui.label(".");
+        });
+        if cfg!(debug_assertions) {
+            ui.separator();
+            egui::warn_if_debug_build(ui);
+        }
+        ui.separator();
         ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
+        ui.hyperlink_to("Source code", "https://github.com/maratik123/biobots");
         ui.label(".");
     });
 }
